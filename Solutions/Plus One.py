20ea1be3adacbcac5a0aba17c7e435b84bb08baa
Omer Ahat <ahatomer45@gmail.com>
2023-07-11 02:10:59 +0300

    Remove All Adjacent Duplicates In String added

--- Solutions/Plus One.py
+++ Solutions/Plus One.py
@@ -1,6 +1,3 @@
 def plusOne(digits):
     number = int(''.join(map(str, digits)))
     return [int(digit) for digit in str(number+1)]
-    
-
-print(plusOne([1,2,4]))
--- Solutions/Remove Element.py
+++ Solutions/Remove Element.py
@@ -6,8 +6,3 @@ def removeElement(nums,val):
     while nums.count(val)!=0:
         nums.remove(val)
     return len(nums)
-
-
-nums = [0,1,2,2,3,0,4,2]
-val = 2
-print(removeElement(nums,val))
